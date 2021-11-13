0070ecc4088d0e8cbf24aab955d80eb7d2bdad55
Carla Ponciano <cponciano@ciandt.com>
2021-11-12 22:11:12 -0300

    Added sequelize configs

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,9 @@
-require('dotenv/config');
-
 module.exports = {
     dialect: 'postgres',
-    host: process.env.PG_HOST,
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
+    host: 'localhost',
+    username: 'postgres',
+    password: 'postgres',
+    database: 'tcc',
     define: {
         timestamps: true,
         underscored: true,
